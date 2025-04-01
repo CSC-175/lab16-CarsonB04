46fad0831605e92bb0783365a1e72018f54ac096
your name <your email>
2025-03-31 23:27:49 -0400

    1st attempt

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,13 @@
 // Code to implement the gcf function goes here
+#include <iostream>
+using namespace std;
+
+int gcf(int a, int b) {
+    // Base case: if b becomes 0, return a as the GCF
+    if (b == 0) {
+        return a;
+    }
+    // Recursive call: apply Euclidean algorithm
+    return gcf(b, a % b);
+}
+
